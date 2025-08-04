315cedc9eb22421775accc8b7b3d477004d46d2b
naadiya sakkeer <naadiyasakkeer@gmail.com>
2025-08-04 23:54:07 +0530

    deploy

--- database/db.js
+++ database/db.js
@@ -1,16 +1,13 @@
 const mongoose = require('mongoose');
-require('dotenv').config(); // Ensure .env is loaded here too
+require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`✅ MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`❌ MongoDB connection error: ${error.message}`);
-    process.exit(1); // Exit process with failure
+    process.exit(1);
   }
 };
 
